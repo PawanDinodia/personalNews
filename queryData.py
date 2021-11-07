5c56138b7faeb76cd21f2f4a78e4be23783c92a1
pawandinodiabme@gmail.com <pawandinodiabme@gmail.com>
2021-11-08 01:11:24 +0530

    job catagory added, removed api bug

--- queryData.py
+++ queryData.py
@@ -23,26 +23,40 @@ def getUpdate(cat_id):
 	
 	query = "SELECT * FROM webUpdates WHERE updateCatagory='"+cat_id+"' AND delFlag!=1;"
 	all_selected_update=crsr1.execute(query).fetchall();
+	# print("__________________________________________________")
+	# print(len(all_selected_update))
+	# print("__________________________________________________")
 	
 	query1 = "SELECT DISTINCT updateUrl FROM webUpdates WHERE updateCatagory='"+cat_id+"' AND delFlag!=1;"
 	sites=crsr1.execute(query1).fetchall()
+	contents["dat"]=all_selected_update;
 
-	for site in sites:
-		sites_list.append(site[0])
-		query2 = "SELECT DISTINCT updateSubCatagory, updateSubUrl FROM webUpdates WHERE updateUrl='"+site[0]+"' AND delFlag!=1;"
-		sub_cats=crsr1.execute(query2).fetchall()
-		# print("__________________________________________________")
-		# print(sub_cats[1])
-		# print("__________________________________________________")
-		for sub_cat in sub_cats:
-			subcats_list.append([sub_cat[0],sub_cat[1]])
-			query3 = "SELECT * FROM webUpdates WHERE updateSubCatagory = '"+sub_cat[0]+"' AND delFlag!=1;"
-			dat=crsr1.execute(query3).fetchall()
-			total_dat["{}".format(sub_cat[0])]=dat
-	contents["updateCatagoryTitle"]=catTitle
-	contents["sites"]=sites_list
-	contents["subcats"]=subcats_list
+	for update in all_selected_update:
+		if update[1] not in sites_list:
+			sites_list.append(update[1])
+		if [update[7],update[12]] not in subcats_list:
+			subcats_list.append([update[7],update[12]])
+			total_dat["{}".format(update[7])]=[]
+	for subcat in subcats_list:
+		for dat in all_selected_update:
+			if dat[7]==subcat[0]:
+				total_dat["{}".format(subcat[0])].append(dat)
+
+	# for site in sites:
+	# 	sites_list.append(site[0])
+	# 	query2 = "SELECT DISTINCT updateSubCatagory, updateSubUrl FROM webUpdates WHERE updateUrl='"+site[0]+"' AND delFlag!=1;"
+	# 	sub_cats=crsr1.execute(query2).fetchall()
+	# 	for sub_cat in sub_cats:
+	# 		subcats_list.append([sub_cat[0],sub_cat[1]])
+	# 		query3 = "SELECT * FROM webUpdates WHERE updateSubCatagory = '"+sub_cat[0]+"' AND delFlag!=1;"
+	# 		dat=crsr1.execute(query3).fetchall()
+	# 		total_dat["{}".format(sub_cat[0])]=dat
+	# contents["sites"]=sites_list
+	# contents["subcats"]=subcats_list
 	contents["dat"]=total_dat
+	contents["subcats"]=subcats_list
+	contents["sites"]=sites_list
+	contents["updateCatagoryTitle"]=catTitle
 	conn1.close();
 	return json.dumps(contents)
 
--- webUpdates/webUpdates/spiders/webUpdateSpider.py
+++ webUpdates/webUpdates/spiders/webUpdateSpider.py
@@ -4,8 +4,10 @@ from datetime import datetime
 
 class WebUpdateSpider(scrapy.Spider):
 	name="webUpdates"
-	start_urls=["https://cdsco.gov.in/opencms/opencms/en/Medical-Device-Diagnostics/Medical-Device-Diagnostics/","https://nehu.ac.in/announcement","https://nehu.ac.in/event"]
-	# start_urls=["https://nehu.ac.in/event"]
+	start_urls=["https://cdsco.gov.in/opencms/opencms/en/Medical-Device-Diagnostics/Medical-Device-Diagnostics/","https://nehu.ac.in/announcement","https://nehu.ac.in/event","https://nehu.ac.in/news-archive","https://nehu.ac.in/vacancy","https://recruitment.nhsrcindia.org/my/job"]
+	# start_urls=["https://nehu.ac.in/vacancy","https://recruitment.nhsrcindia.org/my/job"]
+
+
 #updateSubCatagory should be unique within catagory------without_spaces
 #updateCatagory should be unique within catagory------without_spaces
 #updateCatagoryTitle Try to keep it unique
@@ -63,9 +65,6 @@ class WebUpdateSpider(scrapy.Spider):
 					items["updateDates"]=announcement.css("span::text").extract()[0]
 					items["updateSubCatagory"]="nehu_announcements"
 					items["updateLink"]=announcement.css("a::attr(href)").extract()[0]
-					# print("----------------------------------------------------")
-					# print(items)
-					# print("----------------------------------------------------")
 					yield items
 			elif(response.url.split("/")[3]=="event"):
 				dates=response.css(".clearfix .col-md-9 h4::text").extract()
@@ -77,8 +76,50 @@ class WebUpdateSpider(scrapy.Spider):
 						items["updateDates"]="{} {}".format(event.css("td")[0].css("span b::text").extract()[0],date)
 						items["updateSubCatagory"]="nehu_events"
 						items["updateLink"]=event.css("td")[1].css("a::attr(href)").extract()[0]
-						# print("----------------------------------------------------")
-						# print(items)
-						# print("----------------------------------------------------")
 						yield items
 					cur_event=cur_event+1;
+			elif(response.url.split("/")[3]=="news-archive"):
+				news=response.css(".right-divider h3");
+				dates=response.css(".right-divider p");
+				cur_event=0
+				for news_update in news:
+					items["updateTitle"]=news_update.css("a::text").extract()[0]
+					items["updateDates"]=dates[cur_event*3].css(".news-pubdate::text").extract()[0]
+					items["updateSubCatagory"]="news_update"
+					items["updateLink"]=news_update.css("a::attr(href)").extract()[0]
+					cur_event=cur_event+1
+					yield items
+			elif(response.url.split("/")[3]=="vacancy"):
+				items["updateSubUrl"]=response.url
+				items["updateFound"]=datetime.today().strftime("%d-%b-%Y")
+				items["markNew"]=1
+				items["setImportant"]=0
+				items["updateCatagory"]="jobs"
+				items["updateCatagoryTitle"]="Jobs"
+				vacancies=response.css(".right-divider ol li");
+				for vacancy in vacancies:
+					items["updateTitle"]=vacancy.css("a::text").extract()[0]
+					items["updateDates"]=vacancy.css("code::text").extract()[0]
+					items["updateSubCatagory"]="nehu_jobs"
+					items["updateLink"]=vacancy.css("a::attr(href)").extract()[0]
+					# print("----------------------------------------------------")
+					# print(items)
+					# print("----------------------------------------------------")
+					yield items
+		elif (items["updateUrl"]=="recruitment.nhsrcindia.org"):
+			items["updateSubUrl"]=response.url
+			items["updateFound"]=datetime.today().strftime("%d-%b-%Y")
+			items["markNew"]=1
+			items["setImportant"]=0
+			items["updateCatagory"]="jobs"
+			items["updateCatagoryTitle"]="Jobs"
+			vacancies=response.css("table.table tbody tr");
+			for vacancy in vacancies:
+				print("----------------------------------------------------")
+				print(vacancy.css("td").extract())
+				print("----------------------------------------------------")
+				items["updateTitle"]=vacancy.css("td")[1].css("span::text").extract()[0]
+				items["updateDates"]=vacancy.css("td")[2].css("span::text").extract()[0]
+				items["updateSubCatagory"]="nhsrc_jobs"
+				items["updateLink"]="{}{}".format(response.url,vacancy.css("td")[4].css("a::attr(href)").extract()[0])
+				yield items
